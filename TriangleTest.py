ce7eecc8e9347e56e27d8b64d498b83b2d2bcf9e
shengda567 <57602285+shengda567@users.noreply.github.com>
2020-02-24 19:34:08 -0500

    Update TriangleTest.py

--- TriangleTest.py
+++ TriangleTest.py
@@ -1,35 +1,35 @@
 import unittest
-from .homework1 import classfiy_triangle
+from homework1 import clsfy_triangle
 class TestTriangles(unittest.TestCase):
     def testRightTriangleA(self):
-        self.assertEqual(classfiy_triangle(3, 4, 5), 'Right')
+        self.assertEqual(clsfy_triangle(3, 4, 5), 'Right')
 
     def testRightTriangleB(self):
-        self.assertEqual(classfiy_triangle(5, 3, 4), 'Right')
+        self.assertEqual(clsfy_triangle(5, 3, 4), 'Right')
 
     def testEquilateralTriangles(self):
-        self.assertEqual(classfiy_triangle(1, 1, 1), 'Equilateral')
+        self.assertEqual(clsfy_triangle(1, 1, 1), 'Equilateral')
 
     def testIsoscelesTrianglesA(self):
-        self.assertEqual(classfiy_triangle(5, 5, 7), 'Isosceles')
+        self.assertEqual(clsfy_triangle(5, 5, 7), 'Isosceles')
 
     def testIsoscelesTrianglesB(self):
-        self.assertEqual(classfiy_triangle(5, 7, 7), 'Isosceles')
+        self.assertEqual(clsfy_triangle(5, 7, 7), 'Isosceles')
 
     def testScaleneTriangles(self):
-        self.assertEqual(classfiy_triangle(5, 6, 7), 'Scalene')
+        self.assertEqual(clsfy_triangle(5, 6, 7), 'Scalene')
 
     def testNotATriangle(self):
-        self.assertEqual(classfiy_triangle(3, 4, 10), 'NotATriangle')
+        self.assertEqual(clsfy_triangle(3, 4, 10), 'NotATriangle')
 
     def testInvalidA(self):
-        self.assertEqual(classfiy_triangle(5, 0, 3), "InvalidInput")
+        self.assertEqual(clsfy_triangle(5, 0, 3), "InvalidInput")
 
     def testInvalidB(self):
-        self.assertEqual(classfiy_triangle('I', 203, 162), "InvalidInput")
+        self.assertEqual(clsfy_triangle('I', 203, 162), "InvalidInput")
 
     def testInvalidC(self):
-        self.assertEqual(classfiy_triangle(5.5, 3, 3), "InvalidInput")
+        self.assertEqual(clsfy_triangle(5.5, 3, 3), "InvalidInput")
 
 
 if __name__ == '__main__':
