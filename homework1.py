5b0585d844bb284e10ec18e6eb78b326c86baa23
shengda567 <57602285+shengda567@users.noreply.github.com>
2020-02-24 19:33:28 -0500

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,16 +1,27 @@
-def classfiy_triangle(a, b, c):
-
-    if (isinstance(a, int)) and isinstance(b, int) and isinstance(c, int) and a > 0 and b > 0 and c >0:
-        if (a + b > c and b + c > a and a + c > b):
-            if (a == b == c):
+"""
+classify the trianngle is valid and print the result
+"""
+# classify the trianngle is valid and print the result
+def clsfy_triangle(side1, side2, side3):
+    """
+    :param side1: int
+    :param side2: int
+    :param side3: int
+    :return: string
+    """
+    if isinstance(side1, int) and isinstance(side2, int) and isinstance(side3, int) and \
+            side1 > 0 and side2 > 0 and side3 > 0:
+        if side1 + side2 > side3 and side2 + side3 > side1 and side1 + side3 > side2:
+            if side1 == side2 == side3:
                 return "Equilateral"
-            elif a == b or a == c or b == c:
+            elif side1 == side2 or side1 == side3 or side2 == side3:
                 return "Isosceles"
-            elif a*a + b*b == c*c or  a*a + c*c == b*b or b*b + c*c == a*a:
+            elif side1*side1 + side2*side2 == side3*side3 \
+                    or side1*side1 + side3*side3 == side2*side2 \
+                    or side2*side2 + side3*side3 == side1*side1:
                 return "Right"
             else:
                 return "Scalene"
-
         else:
             return "NotATriangle"
 
@@ -18,4 +29,4 @@ def classfiy_triangle(a, b, c):
 
 
 if __name__ == '__main__':
-    print("classify_triangle(a, b, c)=", classfiy_triangle(5,7,7), sep=" ")
+    print("classify_triangle(side1, side2, side3)=", clsfy_triangle(5, 7, 7), sep=" ")
